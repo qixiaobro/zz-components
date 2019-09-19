5193ede92a8dde060945a0c9e5f4845355ae2deb
qixiaobro <982092690@qq.com>
2019-09-19 11:23:41 +0800

    引入mdui

--- src/main.js
+++ src/main.js
@@ -2,11 +2,15 @@ import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
 import store from './store'
+import mdui from 'mdui' //引入mdui
+import 'mdui/dist/css/mdui.min.css'
+import 'mdui/dist/js/mdui.min.js'
+
 
 Vue.config.productionTip = false
-
 new Vue({
   router,
   store,
+  mdui,
   render: h => h(App)
 }).$mount('#app')
