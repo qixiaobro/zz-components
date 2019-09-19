621be8952af3789beb0bafbd664b414f58553e0d
qixiaobro <982092690@qq.com>
2019-09-19 14:05:18 +0800

    用webpack重建项目

--- src/main.js
+++ src/main.js
@@ -1,16 +1,19 @@
+// The Vue build version to load with the `import` command
+// (runtime-only or standalone) has been set in webpack.base.conf with an alias.
 import Vue from 'vue'
-import App from './App.vue'
+import App from './App'
 import router from './router'
 import store from './store'
-import mdui from 'mdui' //引入mdui
-import 'mdui/dist/css/mdui.min.css'
+import 'mdui/dist/css/mdui.min.css'//引入mdui
 import 'mdui/dist/js/mdui.min.js'
 
-
 Vue.config.productionTip = false
+
+/* eslint-disable no-new */
 new Vue({
+  el: '#app',
   router,
   store,
-  mdui,
-  render: h => h(App)
-}).$mount('#app')
+  components: { App },
+  template: '<App/>'
+})
---
+++ src/store/index.js
@@ -0,0 +1,18 @@
+import Vue from 'vue';
+import Vuex from 'vuex';
+Vue.use(Vuex);
+
+var getters = {
+    state: function (state) {
+        return state;
+    }
+}
+
+export default new Vuex.Store({
+    state:{
+
+    },
+    mutations: {
+  
+    }
+  });
